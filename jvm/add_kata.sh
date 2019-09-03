95098de1aa7a031f778bfe89b6b40c1f42e5b238
Vasile Ungureanu <vungureanu@4suites.nl>
2019-09-03 15:56:47 +0300

    Add 'add_kata' script in jvm category

---
+++ jvm/add_kata.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+#
+# Add a new kata.
+# Thanks to: https://github.com/Egga/katas/blob/master/add_module.sh
+
+# If a command fails then do not proceed and fail this script too
+set -o errexit
+set -o pipefail
+
+read -rp "Please enter the name: " NAME
+echo
+export NAME
+
+if [[ -z "${NAME}" ]]; then
+  echo 'Name was not entered'
+  exit 1
+fi
+
+if [[ -d "src/main/kotlin/${NAME}" ]]; then
+  echo 'Kata with this name already exist'
+  exit 1
+fi
+
+cp -r src/main/kotlin/template/ src/main/kotlin/"${NAME}"/
+mkdir src/test/kotlin/"${NAME}"/
+
+echo "${NAME} kata was added"
+
